0c54ac95ded4464e5ab4510ce5a9aa14f11a52ed
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-11-24 14:28:51 +0100

    Change sorting behavior

--- sort_table.js
+++ sort_table.js
@@ -5,16 +5,21 @@ function makeTableSortable() {
          const comparisonResult = [];
          const maxLength = Math.max(v1.length, v2.length);
          const lengthDiffV1V2 = v1.length - v2.length;
+         const reFromTo = /[/-][^.,]+/;
 
          for (let i=0; i < maxLength; i++) {
 
              let localCompare;
              let v1Current;
              let v2Current;
+             let v1FromTo;
+             let v2FromTo;
 
              try {
-                 v1Current = v1[i].replace(/^(\d+)[a-zA-Z]/, '$1');
-                 v2Current = v2[i].replace(/^(\d+)[a-zA-Z]/, '$1');
+                 v1Current = v1[i].replace(/^(\d+)[a-zA-Z]/, '$1').replace(reFromTo, '');
+                 v1FromTo = v1[i].match(reFromTo);
+                 v2Current = v2[i].replace(/^(\d+)[a-zA-Z]/, '$1').replace(reFromTo, '');
+                 v2FromTo = v2[i].match(reFromTo);
              } catch(error) {
                  //console.log(error);
                  continue;
@@ -28,14 +33,23 @@ function makeTableSortable() {
                      localCompare = v1Current.toString().localeCompare(v2Current);
                  }
 
+                 if (localCompare === 0 && v1FromTo && v2FromTo) {
+                     localCompare = v1[i].localeCompare(v2[i]);
+                 } else if (localCompare === 0 && v1FromTo) {
+                     localCompare = 1;
+                 } else if (localCompare === 0 && v2FromTo) {
+                     localCompare = -1;
+                 }
+
                  comparisonResult.push(localCompare);
              }
          }
+         console.log("v1 " + v1 + " and v2 " + v2 + " resulted in " + comparisonResult);
 
          if ( comparisonResult.every(x => x === 0) ) {
-             if (lengthDiffV1V2 > 0) {
+             if ( lengthDiffV1V2 > 0 || v1[0].match(reFromTo) ) {
                  return 1;
-             } else if (lengthDiffV1V2 < 0) {
+             } else if ( lengthDiffV1V2 < 0 || v2[0].match(reFromTo) ) {
                  return -1;
              } else {
                  return 0;
@@ -61,10 +75,9 @@ function makeTableSortable() {
      let asc;
 
      const reNichtsortier = /<<[^>]*>> */;
-     const reFromTo = /[/-][^.,]+/;
      const reSplitter = /[,. ]/;
 
-     const getCellValue = (tr, idx) => idx == 0 ? tr.children[idx].innerText.replace(reFromTo, '').split(reSplitter) || tr.children[idx].textContent.replace(reFromTo, '').split(reSplitter) : tr.children[idx].innerText.replace(reNichtsortier, '') || tr.children[idx].textContent.replace(reNichtsortier, '');
+     const getCellValue = (tr, idx) => idx == 0 ? tr.children[idx].innerText.split(reSplitter) || tr.children[idx].textContent.split(reSplitter) : tr.children[idx].innerText.replace(reNichtsortier, '') || tr.children[idx].textContent.replace(reNichtsortier, '');
 
      const comparer = (idx, asc) => (a, b) => ((v1, v2) => compareValues(v1, v2)
          )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
